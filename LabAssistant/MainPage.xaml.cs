1221528016ed74996660096e82b70b4fa21f72ae
Lee <leealanberg@gmail.com>
2015-12-12 23:20:34 -0600

    Added debug output for listview selections

--- LabAssistant/MainPage.xaml.cs
+++ LabAssistant/MainPage.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -17,12 +20,10 @@ using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Media.Imaging;
 
 using System.Net;
-using System.Text;
-using System.IO;
-using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using Windows.UI.Xaml.Media.Imaging;
+
+using System.Text;
 
 
 
@@ -38,55 +39,80 @@ namespace LabAssistant
     {
 
         private static HttpResponseMessage response;
-        private string Action;
-
+      
         ObservableCollection<String> Actions = new ObservableCollection<String>();
 
         public MainPage()
         {
 
             this.InitializeComponent();
-            
-            Actions.Add("Arial");
-            Actions.Add("Courier New");
-            Actions.Add("Times New Roman");
-
+         
 
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+   
+        private async void button_Click(object sender, RoutedEventArgs e)
         {
             // GetActionSelection
             // GetLabSelection
 
             // RunActions
+            ExperimentInProgressRing.IsActive = true;
+            // WHICH HEADER DID THEY PICK!
+          
+            string PivotHeaderIndex = LabPivot.SelectedIndex.ToString();
 
-            Debug.WriteLine(ActionListBoxLocal.SelectedIndex.ToString());
-            // 0 = EX 1
-            // 1 = EX 2 
-            // 2 = EX 3
-
-            Debug.WriteLine(LabPivot.SelectedIndex.ToString());
-            Action = LabPivot.SelectedIndex.ToString();
-
-            Windows.UI.Popups.MessageDialog messageDialog =
-             new Windows.UI.Popups.MessageDialog("Thank you for choosing banana.");
-            //await messageDialog.ShowAsync();
-
-
-            if (Action == "1")
+            if (PivotHeaderIndex == "0")
             {
-                //WebHook();
+                Debug.WriteLine("Lab Location is Local");
+
+                if (ActionListBoxLocal.SelectedIndex.ToString() == "0")
+                {
+                    Debug.WriteLine("Single Nano Server Experiment");
+                }
+
+                if (ActionListBoxLocal.SelectedIndex.ToString() == "1")
+                {
+                    Debug.WriteLine("Deploy Many IIS Experiment");
+                }
+
+                if (ActionListBoxLocal.SelectedIndex.ToString() == "2")
+                {
+                    Debug.WriteLine("Deploy SIngle Nano Experiment");
+                }
+
+                
             }
-    
-            // 0 = LOCAL
-            // 1 = CLOUDE!
 
+            if (PivotHeaderIndex == "1")
+            {
+                Debug.WriteLine("Lab Location is Cloud");
 
+                if (ActionListBoxCloud.SelectedIndex.ToString() == "0")
+                {
+                    Debug.WriteLine("Deploy DNS Experiment");
+                }
 
+                if (ActionListBoxCloud.SelectedIndex.ToString() == "1")
+                {
+                    Debug.WriteLine("OMS Lab Deployment Experiment");
+                }
+
+                if (ActionListBoxCloud.SelectedIndex.ToString() == "2")
+                {
+                    Debug.WriteLine("Cloud Linux Text Experiment");
+                }
+
+            }
+
+            Windows.UI.Popups.MessageDialog messageDialog = new Windows.UI.Popups.MessageDialog("Thank you for choosing an experiment.");
+            await messageDialog.ShowAsync();
+
+            ExperimentInProgressRing.IsActive = false;
 
 
         }
+
 
         private async void WebHook()
         {
